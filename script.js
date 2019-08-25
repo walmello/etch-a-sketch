aa0f89eac2c0d745f5e8730a1d75d99287c3813f
Walter Melo <walmello24@gmail.com>
2019-08-24 21:15:17 -0300

    fixed the blank space bug

--- script.js
+++ script.js
@@ -2,23 +2,28 @@ function createSquare(){
     let sqr = document.createElement('div')
     sqr.classList.add('square')
     return sqr
+
+    document.querySelector('#app')
+      .appendChild(sqr)
 }
 
-function createSquaresLine(){
+function createSquareLine(){
     let line = document.createElement('div')
+    line.classList.add('line')
     for(let i = 0; i < 16; i++){
         line.appendChild(createSquare())
     }
-    return line
+    return line 
 }
 
-function createSquaresTable(){
+function createSquareTable(){
     let table = document.createElement('div')
     for(let i = 0; i < 16; i++){
-        table.appendChild(createSquaresLine())
+        table.appendChild(createSquareLine())
     }
     return table
 }
 
-document.querySelector('body')
-   .appendChild(createSquaresTable())
+let app = document.querySelector('#app')
+
+app.appendChild(createSquareTable())
