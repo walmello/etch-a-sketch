268d89e14ca953def875e648c34a92a321313582
Walter Melo <walmello24@gmail.com>
2019-08-25 00:40:23 -0300

    created a README with the link

---
+++ README.md
@@ -0,0 +1,3 @@
+## Etch-a-Sketch exercise
+
+link to my solution here: https://walmello24.github.io/etch-a-sketch/
--- script.js
+++ script.js
@@ -1,29 +1,63 @@
-function createSquare(){
+function createSquare(size=25){
     let sqr = document.createElement('div')
     sqr.classList.add('square')
+    sqr.style.width = size + 'px'
+    sqr.style.height = size + 'px'
+    sqr.addEventListener('mouseover', function(event){
+        changeColor(this)
+    })
     return sqr
 
     document.querySelector('#app')
       .appendChild(sqr)
 }
 
-function createSquareLine(){
+function changeColor(obj){
+    let hue = Math.floor(Math.random() * 360)
+    obj.style.backgroundColor = `hsl(${hue}, 100%, 50%)`
+    let opacity = obj.style.opacity || 1
+    obj.style.opacity = opacity - 0.1
+}
+
+function createSquareLine(size=25, w=16){
     let line = document.createElement('div')
     line.classList.add('line')
-    for(let i = 0; i < 16; i++){
-        line.appendChild(createSquare())
+
+    line.style.height = size + 'px'
+    line.style.width = (size * w) + 'px'
+    for(let i = 0; i < w; i++){
+        line.appendChild(createSquare(size))
     }
     return line 
 }
 
-function createSquareTable(){
+function createSquareTable(w,h,tablesize = 400){
     let table = document.createElement('div')
-    for(let i = 0; i < 16; i++){
-        table.appendChild(createSquareLine())
+    table.classList.add('table')
+    let size = tablesize / (w + h / 2) 
+    for(let i = 0; i < h; i++){
+        table.appendChild(createSquareLine(size, w))
     }
+
+    let app = document.querySelector('#app')
+    app.style.width = (size * w) + 'px'
+
     return table
 }
 
-let app = document.querySelector('#app')
+function newTable(){
+    let w = parseInt(prompt('width in squares?'))
+    let h = parseInt(prompt('height in squares?'))
 
-app.appendChild(createSquareTable())
+    table.innerHTML = ""
+    table.appendChild(createSquareTable(w,h,800))
+}
+
+let table = document.querySelector('#table')
+
+table.appendChild(createSquareTable(50, 50, 800))
+
+
+let clear = document.querySelector('#clear')
+
+clear.addEventListener('click', newTable)
